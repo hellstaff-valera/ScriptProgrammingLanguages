4f4f8cda0e2e44ed5b97ef5cc8ce18b528c04aa3
Valera Kravchenko <49789652+hellstaff-valera@users.noreply.github.com>
2022-10-12 16:22:00 +0300

    create lab 2

---
+++ lab2/proxy/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const config = require('./config.js');
+const client = require('./client.js');
+const app = express();
+
+app.get("/new", function (request, response, next) {
+	client.get_data(config.get('src'))
+		.then(result => {
+			response.send(result);
+		});
+});
+
+app.use("/", function (request, response, next) {
+	client.get_data(config.get('env'))
+		.then(result => {
+			response.send(result);
+		});
+});
+
+module.exports = app;
