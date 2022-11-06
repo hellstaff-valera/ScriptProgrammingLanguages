e4889b18be5d624a808d5e5a626764ced446da99
Valera Kravchenko <49789652+hellstaff-valera@users.noreply.github.com>
2022-11-06 23:40:41 +0200

    create lab4

---
+++ lab4/app.js
@@ -0,0 +1,75 @@
+const mongoose = require("mongoose");
+const express = require("express");
+const Schema = mongoose.Schema;
+const app = express();
+const jsonParser = express.json();
+
+const employeeSchema = new Schema(
+	{
+		name: String,
+		age: Number,
+		solary: Number,
+	},
+	{
+		versionKey: false
+	}
+);
+const Employee = mongoose.model("Employee", employeeSchema);
+
+app.use(express.static(__dirname + "/public"));
+mongoose.connect("mongodb://localhost:27017/employeesdb", { useUnifiedTopology: true, useNewUrlParser: true }, function (err) {
+	if (err) return console.log(err);
+	app.listen(3000, function () {
+		console.log("Сервер чекає з'єднання...");
+	});
+});
+
+app.get("/api/employees", function (req, res) {
+	Employee.find({}, function (err, employees) {
+		if (err) return console.log(err);
+		res.send(employees)
+	});
+});
+
+app.get("/api/employees/:id", function (req, res) {
+	const id = req.params.id;
+	Employee.findOne({ _id: id }, function (err, employee) {
+		if (err) return console.log(err);
+		res.send(employee);
+	});
+});
+
+app.post("/api/employees", jsonParser, function (req, res) {
+	if (!req.body) return res.sendStatus(400);
+	const employeeName = req.body.name;
+	const employeeAge = req.body.age;
+	const employeeSolary = req.body.solary;
+	const employee = new Employee({ name: employeeName, age: employeeAge, solary: employeeSolary });
+	employee.save(function (err) {
+		if (err) return console.log(err);
+		res.send(employee);
+	});
+});
+
+app.delete("/api/employees/:id", function (req, res) {
+	const id = req.params.id;
+	Employee.findByIdAndDelete(id, function (err, employee) {
+		if (err) return console.log(err);
+		res.send(employee);
+	});
+});
+
+app.put("/api/employees", jsonParser, function (req, res) {
+	if (!req.body) return res.sendStatus(400);
+	const id = req.body.id;
+	const employeeName = req.body.name;
+	const employeeAge = req.body.age;
+	const employeeSolary = req.body.solary;
+	const newEmployee = { name: employeeName, age: employeeAge, solary: employeeSolary };
+	Employee.findOneAndUpdate({ _id: id }, newEmployee,
+		{ new: true },
+		function (err, employee) {
+			if (err) return console.log(err);
+			res.send(employee);
+		});
+});
